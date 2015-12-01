5f793fc24886cae8d07e4198639c91c19991fb97
Amanda Oliver <amynahmas@hotmail.es>
2015-12-01 02:08:05 +0100

    widget(no funciona)

---
+++ app/src/main/java/uca/es/estudiantes/Widget.java
@@ -0,0 +1,26 @@
+package uca.es.estudiantes;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+public class Widget extends AppWidgetProvider {
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+                         int[] appWidgetIds) {
+        final int N = appWidgetIds.length;
+        for (int i=0; i<N; i++) {
+            int appWidgetId = appWidgetIds[i];
+            Intent intent = new Intent(context, TodosActivity.class);
+            PendingIntent pendingIntent =
+                    PendingIntent.getActivity(context, 0, intent, 0);
+            RemoteViews views = new RemoteViews(context.getPackageName(),
+                    R.layout.activity_widget);
+            views.setOnClickPendingIntent(R.id.widget, pendingIntent);
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
+}
