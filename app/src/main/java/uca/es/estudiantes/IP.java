75b36479177a444092bbcb74e3314adec7bf45d1
Antojer <anjesarol@gmail.com>
2015-12-06 11:43:48 +0100

    cambio gradle API

--- app/src/main/java/uca/es/estudiantes/IP.java
+++ app/src/main/java/uca/es/estudiantes/IP.java
@@ -1,6 +1,6 @@
 package uca.es.estudiantes;
 
 public class IP {
-    private static String IP="10.181.102.28";//Ip del servidor
+    private static String IP="192.168.0.104";//Ip del servidor
     public static String getIP(){return IP;}
 }
